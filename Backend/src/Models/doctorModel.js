4f6a91b8a675a28b428792b46d4050eb58816d95
Piyush Choudhary <piyush.choudhary6264@gmail.com>
2025-01-01 18:24:25 +0530

    add some new fields in model

--- Backend/src/Models/doctorModel.js
+++ Backend/src/Models/doctorModel.js
@@ -1,6 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
 
-
 const doctorSchema = new Schema({
     name: {
         type: String,
@@ -19,7 +18,7 @@ const doctorSchema = new Schema({
     },
 
     specialization: {
-        type: String,
+        type: [String], // Array to allow multiple specializations
         required: true
     },
 
@@ -38,28 +37,55 @@ const doctorSchema = new Schema({
         required: true
     },
 
-    timings: {
-        startTime: {
-            type: String,
-            required: true
-        },
-        endTime: {
-            type: String,
-            required: true
+    timings: [
+        {
+            day: {
+                type: String,
+                enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+                required: true
+            },
+            startTime: {
+                type: String,
+                required: true
+            },
+            endTime: {
+                type: String,
+                required: true
+            }
         }
-    },
+    ],
 
     availability: {
         type: Boolean,
         default: true
     },
 
+    clinicAddress: {
+        type: String,
+        required: true
+    },
+
+    city: {
+        type: String,
+        required: true
+    },
+
     profilePicture: {
         type: String,
     },
 
-}, {timestamps: true}
-)
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+
+    status: {
+        type: String,
+        enum: ['Active', 'Inactive'],
+        default: 'Active'
+    }
+
+}, {timestamps: true});
 
 const DoctorInfo = mongoose.model('DoctorInfo', doctorSchema);
 
