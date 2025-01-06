d5d953e14858fe34fb5646c214b18267500ef698
Piyush Choudhary <piyush.choudhary6264@gmail.com>
2025-01-06 14:48:20 +0530

    new routes added

--- Backend/src/Routes/appointmentRoute.js
+++ Backend/src/Routes/appointmentRoute.js
@@ -1,23 +1,27 @@
 import express from 'express';
 import {authenticateUser, authorizeRoles} from '../Middlewares/authMiddleware.js'
-import { bookAppointmentController, cancelAppointmentController, getAppointmentByDoctorController, getAppointmentByIdController, getAppointmentByPatientController, getAvailableSlotsController, updateAppointmentsStatusController } from '../Controllers/appointmentController.js';
+import { addVideoCallToHistoryController, bookAppointmentController, cancelAppointmentController, getAppointmentByDoctorController, getAppointmentByIdController, getAppointmentByPatientController, getAvailableSlotsController, getTopReasonController, updateAppointmentsStatusController } from '../Controllers/appointmentController.js';
 
 
 const router = express.Router();
 
 
-router.route('/book').post(authenticateUser, authorizeRoles('Patient'), bookAppointmentController);
+router.route('/book-appointment').post(authenticateUser, authorizeRoles('Patient'), bookAppointmentController);
 
 router.route('/doctor/:doctorId').get(authenticateUser, authorizeRoles('Doctor'), getAppointmentByDoctorController);
 
-router.route('/patient/:patientid').get(authenticateUser, authorizeRoles('Patient'), getAppointmentByPatientController);
+router.route('/patient/:patientId').get(authenticateUser, authorizeRoles('Patient'), getAppointmentByPatientController);
 
-router.route('/status/:appointmentid').put(authenticateUser, authorizeRoles('Doctor'), updateAppointmentsStatusController);
+router.route('/status/:appointmentId').put(authenticateUser, authorizeRoles('Doctor'), updateAppointmentsStatusController);
 
-router.route('/cancel/:appointment').delete(authenticateUser, authorizeRoles('Doctor', 'Patient'), cancelAppointmentController);
+router.route('/cancel/:appointmentId').delete(authenticateUser, authorizeRoles('Doctor', 'Patient'), cancelAppointmentController);
 
 router.route('/get-appointment/:appointmentId').get(authenticateUser, authorizeRoles('Admin', 'Doctor', 'Patient'), getAppointmentByIdController);
 
 router.route('/available-slots').post(authenticateUser, authorizeRoles('Admin', 'Doctor', 'Patient'), getAvailableSlotsController);
+
+router.route('/top-reasons',).get(authenticateUser, authorizeRoles('Admin', 'Doctor',), getTopReasonController);
+
+router.route('/video-call-history').post(authenticateUser, authorizeRoles('Doctor', 'Patient'), addVideoCallToHistoryController)
 
 export default router;
