bf319bc67a21f92d7dc23de6b720916b2209410d
Piyush Choudhary <piyush.choudhary6264@gmail.com>
2025-01-16 11:58:22 +0530

    handle error

--- Backend/src/Middlewares/multerMiddleware.js
+++ Backend/src/Middlewares/multerMiddleware.js
@@ -1,30 +1,29 @@
 import multer from "multer";
-import path from 'path';
+import path from "path";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+        cb(null, "./public/temp"); // Ensure this directory exists
     },
     filename: function (req, file, cb) {
-      
-      cb(null, Date.now() + '-' + file.originalname)
-    }
-  })
+        cb(null, Date.now() + '-' + file.originalname);
+    },
+});
 
-  const fileFilter = (req, fole, cb) => {
+const fileFilter = (req, file, cb) => {
     const fileTypes = /jpeg|jpg|png|gif/;
     const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = fileTypes.test(file.mimetype); 
+    const mimetype = fileTypes.test(file.mimetype);
 
-    if(extname && mimetype) {
+    if (extname && mimetype) {
         return cb(null, true);
-      }else{
-        cb(new Error('Only image files are allowed!'), false)
-      }
-  };
+    } else {
+        cb(new Error("Only image files are allowed!"), false);
+    }
+};
 
-export const upload = multer({ 
-    storage, 
+export const upload = multer({
+    storage,
     fileFilter,
-    limits: { fileSize: 5 * 1024 * 1024}
-}).single('profilePicture');
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+});
